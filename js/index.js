ebcc366edc96d7eecb776abbc0018dc18ec13411
Tjandra Putra <tjandraputra2000@gmail.com>
2022-06-03 16:29:49 +0800

    update: carousel dragon to scroll

--- js/index.js
+++ js/index.js
@@ -231,3 +231,29 @@ new hoverEffect({
 
 // ========================== GET CURRENT YEAR FOR COPYRIGHT ==========================
 document.getElementById("copyright").appendChild(document.createTextNode(new Date().getFullYear()));
+
+// ========================== CAROUSEL DRAG TO SCROLL ==========================
+const slider = document.querySelector(".carousel");
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener("mousedown", (e) => {
+  isDown = true;
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener("mouseleave", () => {
+  isDown = false;
+});
+slider.addEventListener("mouseup", () => {
+  isDown = false;
+});
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 3; //scroll-fast
+  slider.scrollLeft = scrollLeft - walk;
+  console.log(walk);
+});
